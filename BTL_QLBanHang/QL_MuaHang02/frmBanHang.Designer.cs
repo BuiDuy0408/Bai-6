cbe19b9594a90ecc90855f23f9cbfed367ccb705
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-26 16:29:44 +0700

    thanh-add picture box

--- BTL_QLBanHang/QL_MuaHang02/frmBanHang.Designer.cs
+++ BTL_QLBanHang/QL_MuaHang02/frmBanHang.Designer.cs
@@ -28,19 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.picSanPham = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picSanPham)).BeginInit();
             this.SuspendLayout();
+            // 
+            // picSanPham
+            // 
+            this.picSanPham.BackColor = System.Drawing.Color.Silver;
+            this.picSanPham.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picSanPham.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picSanPham.Location = new System.Drawing.Point(269, 26);
+            this.picSanPham.Name = "picSanPham";
+            this.picSanPham.Size = new System.Drawing.Size(142, 164);
+            this.picSanPham.TabIndex = 1;
+            this.picSanPham.TabStop = false;
             // 
             // frmBanHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(665, 291);
+            this.Controls.Add(this.picSanPham);
             this.Name = "frmBanHang";
             this.Text = "frmBanHang";
+            ((System.ComponentModel.ISupportInitialize)(this.picSanPham)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picSanPham;
     }
 }
