8b12108eec86928e13c551085dd6ca22a9fafe34
truongmta190298 <camamidol@gmail.com>
2019-11-27 20:45:37 +0700

    ductruong edit form

--- BTL_QLBanHang/QL_MuaHang02/frmKhachHang.Designer.cs
+++ BTL_QLBanHang/QL_MuaHang02/frmKhachHang.Designer.cs
@@ -33,6 +33,14 @@
             this.btnXoa = new System.Windows.Forms.Button();
             this.btnSua = new System.Windows.Forms.Button();
             this.btnLuu = new System.Windows.Forms.Button();
+            this.txttenKH = new System.Windows.Forms.TextBox();
+            this.txtMaKH = new System.Windows.Forms.TextBox();
+            this.lblTenKH = new System.Windows.Forms.Label();
+            this.lblMaKH = new System.Windows.Forms.Label();
+            this.cmbGioiTinh = new System.Windows.Forms.ComboBox();
+            this.lblGioiTinh = new System.Windows.Forms.Label();
+            this.cmbLoaiKH = new System.Windows.Forms.ComboBox();
+            this.lblLoaiKH = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnThem
@@ -96,11 +104,101 @@
             this.btnLuu.Text = "Lưu";
             this.btnLuu.UseVisualStyleBackColor = false;
             // 
+            // txttenKH
+            // 
+            this.txttenKH.Enabled = false;
+            this.txttenKH.Location = new System.Drawing.Point(313, 58);
+            this.txttenKH.Name = "txttenKH";
+            this.txttenKH.Size = new System.Drawing.Size(209, 22);
+            this.txttenKH.TabIndex = 17;
+            // 
+            // txtMaKH
+            // 
+            this.txtMaKH.Enabled = false;
+            this.txtMaKH.Location = new System.Drawing.Point(309, 25);
+            this.txtMaKH.Name = "txtMaKH";
+            this.txtMaKH.Size = new System.Drawing.Size(209, 22);
+            this.txtMaKH.TabIndex = 16;
+            // 
+            // lblTenKH
+            // 
+            this.lblTenKH.AutoSize = true;
+            this.lblTenKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenKH.Location = new System.Drawing.Point(157, 56);
+            this.lblTenKH.Name = "lblTenKH";
+            this.lblTenKH.Size = new System.Drawing.Size(150, 23);
+            this.lblTenKH.TabIndex = 15;
+            this.lblTenKH.Text = "Tên Khách Hàng";
+            // 
+            // lblMaKH
+            // 
+            this.lblMaKH.AutoSize = true;
+            this.lblMaKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaKH.Location = new System.Drawing.Point(157, 21);
+            this.lblMaKH.Name = "lblMaKH";
+            this.lblMaKH.Size = new System.Drawing.Size(146, 23);
+            this.lblMaKH.TabIndex = 14;
+            this.lblMaKH.Text = "Mã Khách Hàng";
+            // 
+            // cmbGioiTinh
+            // 
+            this.cmbGioiTinh.FormattingEnabled = true;
+            this.cmbGioiTinh.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ",
+            "Gay",
+            "Less"});
+            this.cmbGioiTinh.Location = new System.Drawing.Point(329, 83);
+            this.cmbGioiTinh.Name = "cmbGioiTinh";
+            this.cmbGioiTinh.Size = new System.Drawing.Size(121, 24);
+            this.cmbGioiTinh.TabIndex = 25;
+            // 
+            // lblGioiTinh
+            // 
+            this.lblGioiTinh.AutoSize = true;
+            this.lblGioiTinh.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGioiTinh.Location = new System.Drawing.Point(171, 88);
+            this.lblGioiTinh.Name = "lblGioiTinh";
+            this.lblGioiTinh.Size = new System.Drawing.Size(91, 23);
+            this.lblGioiTinh.TabIndex = 24;
+            this.lblGioiTinh.Text = "Giới Tính";
+            // 
+            // cmbLoaiKH
+            // 
+            this.cmbLoaiKH.Enabled = false;
+            this.cmbLoaiKH.FormattingEnabled = true;
+            this.cmbLoaiKH.Items.AddRange(new object[] {
+            "Khách VIP",
+            "Khách Thường Xuyên",
+            "Khách Lẻ"});
+            this.cmbLoaiKH.Location = new System.Drawing.Point(333, 118);
+            this.cmbLoaiKH.Name = "cmbLoaiKH";
+            this.cmbLoaiKH.Size = new System.Drawing.Size(163, 24);
+            this.cmbLoaiKH.TabIndex = 23;
+            // 
+            // lblLoaiKH
+            // 
+            this.lblLoaiKH.AutoSize = true;
+            this.lblLoaiKH.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoaiKH.Location = new System.Drawing.Point(171, 118);
+            this.lblLoaiKH.Name = "lblLoaiKH";
+            this.lblLoaiKH.Size = new System.Drawing.Size(156, 23);
+            this.lblLoaiKH.TabIndex = 22;
+            this.lblLoaiKH.Text = "Loại Khách Hàng";
+            // 
             // frmKhachHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1352, 450);
+            this.Controls.Add(this.cmbGioiTinh);
+            this.Controls.Add(this.lblGioiTinh);
+            this.Controls.Add(this.cmbLoaiKH);
+            this.Controls.Add(this.lblLoaiKH);
+            this.Controls.Add(this.txttenKH);
+            this.Controls.Add(this.txtMaKH);
+            this.Controls.Add(this.lblTenKH);
+            this.Controls.Add(this.lblMaKH);
             this.Controls.Add(this.btnXoa);
             this.Controls.Add(this.btnSua);
             this.Controls.Add(this.btnLuu);
@@ -108,6 +206,7 @@
             this.Name = "frmKhachHang";
             this.Text = "frmKhachHang";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -117,5 +216,13 @@
         private System.Windows.Forms.Button btnXoa;
         private System.Windows.Forms.Button btnSua;
         private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.TextBox txttenKH;
+        private System.Windows.Forms.TextBox txtMaKH;
+        private System.Windows.Forms.Label lblTenKH;
+        private System.Windows.Forms.Label lblMaKH;
+        private System.Windows.Forms.ComboBox cmbGioiTinh;
+        private System.Windows.Forms.Label lblGioiTinh;
+        private System.Windows.Forms.ComboBox cmbLoaiKH;
+        private System.Windows.Forms.Label lblLoaiKH;
     }
 }
